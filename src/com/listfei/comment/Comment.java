abc007b34bded6f2097b4942b3264ab1fbd2344b
刘鹏飞 <2480317622@qq.com>
2025-10-26 15:49:12 +0800

    feat: 注释学习

---
+++ src/com/listfei/comment/Comment.java
@@ -0,0 +1,18 @@
+package com.listfei.comment;
+
+/**
+ * javadoc 注释
+ * Created by listfei on 2017/7/20.
+ */
+public class Comment {
+    public static void main(String[] args) {
+        /*
+         * 这是一个多行注释
+        * 输出 Hello World!
+         */
+        System.out.println("多行注释");
+
+        // 单行注释
+        System.out.println("单行注释");
+    }
+}
